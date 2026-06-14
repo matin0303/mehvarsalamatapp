cca707432ca52478d1e2da8b6c8d78a1c2d9556d
matin0303 <pirmohammadimatin@gmail.com>
2026-06-14 18:57:48 +0330

    fix 404

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { vitePages } from '@kingironman2011/vite-pages'
+import { githubPagesSpa } from "@sctg/vite-plugin-github-pages-spa";
 
 export default defineConfig({
-  base: '/mehvarsalamatapp/', 
+  base: '/mehvarsalamatapp/',
   plugins: [
     react(),
-    vitePages(),
+    githubPagesSpa({ verbose: true }),
   ],
 })
